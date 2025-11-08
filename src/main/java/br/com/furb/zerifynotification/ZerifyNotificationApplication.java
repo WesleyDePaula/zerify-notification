8dae2db03cfb6a3001e03e35a2bfde87e245ee77
WesleyDePaula <trexnation1405@gmail.com>
2025-11-08 15:42:52 -0300

    Criação do projeto

---
+++ src/main/java/br/com/furb/zerifynotification/ZerifyNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.furb.zerifynotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerifyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerifyNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/furb/zerifynotification/ZerifyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.furb.zerifynotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerifyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
